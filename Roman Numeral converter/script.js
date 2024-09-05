18d0806f9b0c89b6cb75386e0f2319f14f78de83
oyinkandev <126412485+oyinkandev@users.noreply.github.com>
2024-09-05 19:14:44 +0100

    Add files via upload

---
+++ Roman Numeral converter/script.js
@@ -0,0 +1,42 @@
+const convertBtn = document.getElementById('convert-btn');
+    const output = document.getElementById('output');
+    const numberInput = document.getElementById('number');
+
+    convertBtn.addEventListener('click', () => {
+      const inputValue = numberInput.value.trim();
+      if (inputValue === '') {
+        output.textContent = 'Please enter a valid number';
+      }else if (parseInt(inputValue) < 1) {
+        output.textContent = 'Please enter a number greater than or equal to 1';
+      }else if (parseInt(inputValue) > 3999) {
+        output.textContent = 'Please enter a number less than or equal to 3999';
+      }else {
+        const romanNumerals = [
+          { value: 1000, symbol: 'M' },
+          { value: 900, symbol: 'CM' },
+          { value: 500, symbol: 'D' },
+          { value: 400, symbol: 'CD' },
+          { value: 100, symbol: 'C' },
+          { value: 90, symbol: 'XC' },
+          { value: 50, symbol: 'L' },
+          { value: 40, symbol: 'XL' },
+          { value: 10, symbol: 'X' },
+          { value: 9, symbol: 'IX' },
+          { value: 5, symbol: 'V' },
+          { value: 4, symbol: 'IV' },
+          { value: 1, symbol: 'I' }
+        ];
+
+        let result = '';
+        let number = parseInt(inputValue);
+
+        for (let i = 0; i < romanNumerals.length; i++) {
+          while (number >= romanNumerals[i].value) {
+            result += romanNumerals[i].symbol;
+            number -= romanNumerals[i].value;
+          }
+        }
+
+        output.textContent = result;
+      }
+    });
